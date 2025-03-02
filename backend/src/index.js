59395f6646aff932602e894952e0011c69a9add6
JohnMP <j_ponce3@u.pacific.edu>
2025-03-02 00:19:01 -0800

    feat: added mongodb database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Set up authentication routes with "/api/auth" as the base URL
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("server is running on port 5001");
+// Start the server and establish database connection
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+  connectDB();
 });
